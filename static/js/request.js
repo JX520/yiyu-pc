61d84f7ba5f18c2c22d6fff1d81856cd30b5dac9
shixian <1145024061@qq.com>
2020-09-02 18:58:16 +0800

    09.02

--- static/js/request.js
+++ static/js/request.js
@@ -20,7 +20,7 @@ function AGet(url) {
 }
 
 function Get(url, params) {
-	
+
 	function getConfig(url) {
 		let config = axios.create({
 			baseURL: 'http://127.0.0.1:3000',
@@ -37,11 +37,12 @@ function Get(url, params) {
 
 
 function Post(url, data) {
-	
+
 	function getConfig(url) {
 		let config = axios.create({
 			// baseURL: 'http://127.0.0.1:3000',
-			baseURL:'http://122.51.151.42:8092',
+			// baseURL: 'http://122.51.151.42:8092',
+			baseURL: 'https://api.ranjoin.cn:6092',
 			timeout: 8000
 		})
 
@@ -50,10 +51,10 @@ function Post(url, data) {
 			method: 'post',
 			data: data,
 			header: {
-                // "content-type": "application/x-www-form-urlencoded",
-                "content-type": "application/json",
-                // token: uni.getStorageSync("token")
-            },
+				// "content-type": "application/x-www-form-urlencoded",
+				"content-type": "application/json",
+				// token: uni.getStorageSync("token")
+			},
 		})
 	}
 	return getConfig(url)
